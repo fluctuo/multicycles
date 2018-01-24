576f4deac8e80c545821240a7a14a1de2dec54fd
Pierrick <mulotmail@gmail.com>
2018-01-24 09:57:24 +0100

    Add mobike on Paris

--- api/providersLocations.js
+++ api/providersLocations.js
@@ -1,7 +1,7 @@
 export default {
   // France
-  France: ['ofo', 'gobee'],
-  Paris: ['ofo', 'gobee'],
+  France: ['ofo', 'gobee', 'mobike'],
+  Paris: ['ofo', 'gobee', 'mobike'],
   // China
   Shanghai: ['ofo', 'mobike'],
   // UK
