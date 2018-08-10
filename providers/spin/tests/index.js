0ca2b7b34700173f0b62b1b8dce8b583cc312def
Pierrick <mulotmail@gmail.com>
2018-08-10 14:13:16 +0200

    [PROVIDERS] Fix Spin test

--- providers/spin/tests/index.js
+++ providers/spin/tests/index.js
@@ -43,8 +43,8 @@ test('get bicycles by positions', async t => {
 
   await spin
     .getBicyclesByLatLng({
-      lat: 38.907192,
-      lng: -77.036871,
+      lat: 47.606209,
+      lng: -122.332069,
       distance: 1000
     })
     .then(result => {
