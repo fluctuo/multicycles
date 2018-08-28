8fd2fd83ddbc586db7f710ab5c9d4a753f37bedc
Pierrick <mulotmail@gmail.com>
2018-08-28 15:51:56 +0200

    [API] Add prefix on metrics

--- api/metrics.js
+++ api/metrics.js
@@ -3,20 +3,20 @@ import client from 'prom-client'
 client.collectDefaultMetrics({ prefix: 'api_' })
 
 const vehiclesRequestsTotal = new client.Counter({
-  name: 'vehicles_requests_total',
+  name: 'api_vehicles_requests_total',
   help: 'Total of vehicles query',
   labelNames: ['provider', 'cached']
 })
 
 const vehiclesRequestsDurationSeconds = new client.Histogram({
-  name: 'vehicles_requests_duration_seconds',
+  name: 'api_vehicles_requests_duration_seconds',
   help: 'Vehicles query duration in seconds',
   labelNames: ['provider'],
   buckets: [0.5, 1, 2, 3, 5, 5]
 })
 
 const providersRequestsTotal = new client.Counter({
-  name: 'providers_requests_total',
+  name: 'api_providers_requests_total',
   help: 'Total of providers query',
   labelNames: ['with_lat_lng']
 })
