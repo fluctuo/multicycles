9bba9f8718fd01ec7e92cb5c49bdcc06ef77c158
Pierrick <mulotmail@gmail.com>
2018-11-12 14:58:33 +0100

    [DEVELOPER] Finaly fix GA ahahah

--- developer/plugins/ga.js
+++ developer/plugins/ga.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 
-if (process.env.ANALYTICS_KEY) {
-  Vue.use(VueAnalytics, {
-    id: process.env.ANALYTICS_KEY
-  })
+export default ctx => {
+  const analyticsKey = process.env.ANALYTICS_KEY || ctx.store.state.env.ANALYTICS_KEY
+  if (analyticsKey) {
+    Vue.use(VueAnalytics, {
+      id: analyticsKey,
+      router: ctx.app.router
+    })
+  }
 }
