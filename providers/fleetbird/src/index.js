a7286e0cc5f83c1fc3c593a68912dade0ee747e0
Pierrick <mulotmail@gmail.com>
2018-12-21 12:11:10 +0100

    [PROVIDERS] Fleetbird connecter

---
+++ providers/fleetbird/src/index.js
@@ -0,0 +1,20 @@
+import got from 'got'
+
+class Fleetbird {
+  constructor({ timeout, url } = {}) {
+    this.url = url
+    this.config = {
+      timeout: timeout && parseInt(timeout, 10)
+    }
+  }
+
+  getCars(config = {}) {
+    return got.get(`${this.url}/cars`, {
+      json: true,
+      timeout: this.config.timeout,
+      ...config
+    })
+  }
+}
+
+export default Fleetbird
