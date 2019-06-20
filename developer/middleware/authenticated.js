ad776341a211072da769c429077969f4af6f1c37
Pierrick <mulotmail@gmail.com>
2019-06-20 18:47:15 +0200

    [DEVELOPER] Fix auth middleware

--- developer/middleware/authenticated.js
+++ developer/middleware/authenticated.js
@@ -1,7 +1,5 @@
 export default function({ store, redirect }) {
-  if (!process.server) {
-    if (!store.state.auth) {
-      redirect('/login')
-    }
+  if (!store.state.auth) {
+    redirect('/login')
   }
 }
