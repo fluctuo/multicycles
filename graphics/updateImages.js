0538b83f4881255ad11b0a5d5a42314376b3b208
tdelmas <github@tdelmas.eu>
2020-05-04 17:20:43 +0200

    wip

--- graphics/updateImages.js
+++ graphics/updateImages.js
@@ -5,19 +5,16 @@ const base_path = './providers_base'
 const providers = readdirSync(base_path).map(p => p.substr(0, p.length - 4))
 
 ;(async function() {
-  const x = 176
-  const y = 196
+  const x = 880
+  const y = 980
 
-  const shadow_pos = 70
+  const shadow_pos = 70 * 2
 
   // 176x196
-  const masque = await Jimp.read('./calques/Masque-blanc-x2.png')
+  const masque = await Jimp.read('./calques/Masque-blanc-880x980.png')
 
   // 90x44
-  const shadow = await Jimp.read('./calques/Ombre@2x.png')
-  const markerShadow = new Jimp(x, y, 0xffffffff)
-  markerShadow.opacity(0)
-  markerShadow.composite(shadow, (x - 90) / 2, y - shadow_pos)
+  const shadow = await Jimp.read('./calques/other.png')
 
   const xM = 48
   const yM = 80
@@ -36,11 +33,13 @@ const providers = readdirSync(base_path).map(p => p.substr(0, p.length - 4))
 
     provider_image = provider_image.clone().resize(size, (size * 1050) / 631)
 
-    marker.composite(provider_image, (x - size) / 2, shift_provider)
+    marker.composite(provider_image, (x - size) / 2, shift_provider, {
+      mode: Jimp.BLEND_MULTIPLY
+    })
 
     marker.mask(masque)
 
-    const provider_image_with_shadow = markerShadow.clone()
+    const provider_image_with_shadow = shadow.clone()
     provider_image_with_shadow.composite(marker, 0, 0)
 
     return provider_image_with_shadow
@@ -49,15 +48,15 @@ const providers = readdirSync(base_path).map(p => p.substr(0, p.length - 4))
   for (const provider of [providers[0]]) {
     try {
       const provider_image = await Jimp.read(`${base_path}/${provider}.png`) // 631*1050
-      const size = 80
-      const shift_provider = 40
+      const size = 80 * 5
+      const shift_provider = 40 * 5
 
-      const img = do_marker(provider_image, x, y, size, masque, markerShadow, shift_provider)
-      img.write(`./test/${provider}.png`)
-      /*
-      img.resize(x / 2, y / 2)
+      const img = do_marker(provider_image, 880, 980, size, masque, shadow, shift_provider)
+      img.resize(176, 196)
       img.write(`./test/${provider}-2x.png`)
-*/
+      img.resize(88, 89)
+      img.write(`./test/${provider}.png`)
+
       //provider_image.resize(xM, yM)
       const imgM = do_marker(provider_image, xM, yM, xM, masqueMulticycles, markerShadowMulticycles, 0)
       imgM.write(`./test/${provider}-multicycles-2x.png`)
