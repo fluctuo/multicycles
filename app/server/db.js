5d27e7e998a8a312c8fde1d6553d6c56702f4cdb
Pierrick <mulotmail@gmail.com>
2020-11-04 10:53:50 +0100

    Add DATABASE_PORT

--- app/server/db.js
+++ app/server/db.js
@@ -7,6 +7,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT || 5432,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME || 'multicycles-app'
