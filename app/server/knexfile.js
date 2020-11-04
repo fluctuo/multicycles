a76ecea9d41e471fcbc7c0370114e4477de3b082
Pierrick <mulotmail@gmail.com>
2020-11-04 11:15:47 +0100

    Fix missing PORT on migration

--- app/server/knexfile.js
+++ app/server/knexfile.js
@@ -5,6 +5,7 @@ module.exports = {
   client: 'pg',
   connection: {
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT || 5432,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME || 'multicycles-app'
